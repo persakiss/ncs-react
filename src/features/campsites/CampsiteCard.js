7283a35f1b9590812c7586980e71d88f54d94adb
Arman <persakiss@gmail.com>
2022-06-12 21:49:36 +0400

    destructuring

--- src/features/campsites/CampsiteCard.js
+++ src/features/campsites/CampsiteCard.js
@@ -1,15 +1,19 @@
 import { Card, CardImg, CardImgOverlay, CardTitle } from "reactstrap";
 
-const CampsiteCard = (props) => {
+
+{/* To destructure, changed props to {campsite} in the function param, then was able to remove props in the src/alt/cardtitle call.
+To destructure yet again, assigned const {image, name} = campsite, then was able to remove the campsite reference in those same lines (src/alt/cardtitle*/}
+const CampsiteCard = ({campsite}) => {
+  const {image, name} = campsite;
   return(
     <Card>
       <CardImg 
         width="100%"
-        src={props.campsite.image}
-        alt={props.campsite.name}
+        src={image}
+        alt={name}
       />
       <CardImgOverlay>
-        <CardTitle>{props.campsite.name}</CardTitle>
+        <CardTitle>{name}</CardTitle>
       </CardImgOverlay>
     </Card>
   )
